147b66900188b00b787780e0e0f874c3e32e68db
Zahar Trofymenko <scoolnick031206@gmail.com>
2022-04-12 15:14:07 +0300

    add enterNumber

--- aboba.cpp
+++ aboba.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
+
+int enterNumber(string text) {
+    int num;
+    cout << text;
+    cin >> num;
+    return num;
+}
 
 int main() {
     
