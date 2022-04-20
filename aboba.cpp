8d55f5c0e2a22a64a09f94b114d50b5eff7f580e
Zahar Trofymenko <scoolnick031206@gmail.com>
2022-04-20 10:55:52 +0300

    no changes

--- aboba.cpp
+++ aboba.cpp
@@ -1,16 +1,7 @@
 #include <iostream>
-#include <string>
 
 using namespace std;
 
-int enterNumber(string text) {
-    int num;
-    cout << text;
-    cin >> num;
-    return num;
-}
-
 int main() {
-    int x1 = enterNumber("Введіть перве число: "), x2 = enterNumber("Введіть друге число: ");
-    cout << x1 << " + " << x2 << " = " << x1+x2 << endl;
+    
 }
